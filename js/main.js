8ff29a583a6d4a8be722459714b8ed064ee4f21e
hazerman <49827421+hazerman@users.noreply.github.com>
2020-03-13 16:09:25 +0500

    Добавляет обработчик клика по фото пользователей

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@
 
 (function () {
   var picturesArea = document.querySelector('.pictures');
+  var bigPicture = document.querySelector('.big-picture');
   var usersData = [];
+  var renderedPictures = [];
+
+  picturesArea.addEventListener('click', function (evt) {
+    var target = evt.target.closest('.picture');
+    if (target) {
+      var index = Array.from(renderedPictures).indexOf(target);
+      document.body.className = 'modal-open';
+      bigPicture.classList.remove('hidden');
+      var bigPictureImg = bigPicture.querySelector('.big-picture__img img');
+      bigPictureImg.src = usersData[index].url;
+      bigPictureImg.alt = usersData[index].description;
+    }
+  });
 
   var picturesDownloadSuccessHandler = function (data) {
     var fragmentForPictures = document.createDocumentFragment();
@@ -11,8 +25,9 @@
       var picture = window.picture.render(item);
       fragmentForPictures.append(picture);
     });
+    renderedPictures = fragmentForPictures.querySelectorAll('.picture');
     picturesArea.append(fragmentForPictures);
   };
 
-  window.ajax.downloadData(picturesDownloadSuccessHandler)
+  window.ajax.downloadData(picturesDownloadSuccessHandler);
 })();
--- js/picture.js
+++ js/picture.js
@@ -16,5 +16,5 @@
 
   window.picture = {
     render: renderPicture
-  }
+  };
 })();
