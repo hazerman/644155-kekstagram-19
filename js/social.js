d92185aac1d80933342ff3a288264606de279833
hazerman <49827421+hazerman@users.noreply.github.com>
2020-03-13 22:42:53 +0500

    Меняет архитектуру рендеринга комментариев

--- js/social.js
+++ js/social.js
@@ -7,68 +7,68 @@
   var socialCommentsWrapper = socialArea.querySelector('.social__comments');
   var socialCommentTemplate = socialCommentsWrapper.firstElementChild.cloneNode(true);
   var moreCommentsButton = socialArea.querySelector('.social__comments-loader');
-  var comments = [];
-  var startComment = 0;
-  var endComment = 0;
-
-  var getSlicedCommentArray = function () {
-    endComment += MAX_COMMENTS;
-    endComment = endComment >= comments.length ? comments.length : endComment;
-    var slicedComments = comments.slice(startComment, endComment);
-    startComment += MAX_COMMENTS;
-    if (endComment >= comments.length) {
-      moreCommentsButton.classList.add('hidden');
-      moreCommentsButton.removeEventListener('click', moreCommentsButtonClickHandler);
+  var comment = {
+    messages: [],
+    start: 0,
+    end: 0,
+    getSlicedMessages: function () {
+      this.end += MAX_COMMENTS;
+      this.end = this.end >= this.messages.length ? this.messages.length : this.end;
+      var slicedMessages = this.messages.slice(this.start, this.end);
+      this.start += MAX_COMMENTS;
+      if (this.end >= this.messages.length) {
+        moreCommentsButton.classList.add('hidden');
+        moreCommentsButton.removeEventListener('click', moreCommentsButtonClickHandler);
+      }
+      return slicedMessages;
+    },
+    getRenderedSingle: function (commentData) {
+      var renderedComment = socialCommentTemplate.cloneNode(true);
+      var renderedCommentImg = renderedComment.querySelector('img');
+      var renderedCommentText = renderedComment.querySelector('p');
+      renderedCommentImg.src = commentData.avatar;
+      renderedCommentImg.alt = commentData.name;
+      renderedCommentText.textContent = commentData.message;
+      return renderedComment;
+    },
+    render: function () {
+      var commentsData = this.getSlicedMessages();
+      var fragmentForComments = document.createDocumentFragment();
+      commentsData.forEach(function (item) {
+        fragmentForComments.append(comment.getRenderedSingle(item));
+      });
+      socialCommentsWrapper.append(fragmentForComments);
+      var socialCommentCount = socialArea.querySelector('.social__comment-count');
+      var splittedSocialCommentCount = socialCommentCount.innerHTML.split(' ');
+      splittedSocialCommentCount.splice(0, 1, this.end);
+      socialCommentCount.innerHTML = splittedSocialCommentCount.join(' ');
     }
-    return slicedComments;
-  };
-
-  var getRenderedComment = function (commentData) {
-    var renderedComment = socialCommentTemplate.cloneNode(true);
-    var renderedCommentImg = renderedComment.querySelector('img');
-    var renderedCommentText = renderedComment.querySelector('p');
-    renderedCommentImg.src = commentData.avatar;
-    renderedCommentImg.alt = commentData.name;
-    renderedCommentText.textContent = commentData.message;
-    return renderedComment;
-  };
-
-  var renderComments = function (commentsData) {
-    var fragmentForComments = document.createDocumentFragment();
-    commentsData.forEach(function (item) {
-      fragmentForComments.append(getRenderedComment(item));
-    });
-    socialCommentsWrapper.append(fragmentForComments);
-    var socialCommentCount = socialArea.querySelector('.social__comment-count');
-    var splittedSocialCommentCount = socialCommentCount.innerHTML.split(' ');
-    splittedSocialCommentCount.splice(0, 1, endComment);
-    socialCommentCount.innerHTML = splittedSocialCommentCount.join(' ');
   };
 
   var moreCommentsButtonClickHandler = function () {
-    renderComments(getSlicedCommentArray());
+    comment.render();
   };
 
   var likesCount = document.querySelector('.likes-count');
 
   var renderSocial = function (data) {
     var commentsCount = document.querySelector('.comments-count');
-    comments = data.comments.slice();
+    comment.messages = data.comments.slice();
     socialCaption.textContent = data.description;
     likesCount.textContent = data.likes;
-    commentsCount.textContent = comments.length;
+    commentsCount.textContent = comment.messages.length;
     socialCommentsWrapper.innerHTML = '';
-    renderComments(getSlicedCommentArray());
+    comment.render();
     moreCommentsButton.addEventListener('click', moreCommentsButtonClickHandler);
   };
 
   var resetSocial = function () {
-    if (endComment >= comments.length) {
+    if (comment.end >= comment.messages.length) {
       moreCommentsButton.classList.remove('hidden');
       moreCommentsButton.removeEventListener('click', moreCommentsButtonClickHandler);
     }
-    startComment = 0;
-    endComment = 0;
+    comment.start = 0;
+    comment.end = 0;
   };
 
   window.social = {
